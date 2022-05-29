cb56d7797a7040126ce91b1c8879e8d6b0188516
wxl <511459264@qq.com>
2022-05-29 15:06:18 +0800

    first commit

---
+++ tt.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "ysgg"
+	var b int = 1
+	fmt.Println(a)
+	fmt.Println(b)
+	for j := 7; j < 9; j++ {
+		fmt.Println("ysg")
+	}
+	s := make([]string, 3)
+	s[0] = "a"
+
+}
